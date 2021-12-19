666321a62159fbfd90d49618eba02d47da7c15ce
asaftk1 <93122554+asaftk1@users.noreply.github.com>
2021-12-19 17:39:54 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "func.c"
+#include "my_func.c"
 #define TXT 1024
 #define WORD 30
 
@@ -9,14 +9,26 @@ int main()
 {
     char* a = NULL;
     char* b = NULL;
-    
-    a = (char*)malloc((WORD)*sizeof(char));
-    b = (char*)malloc(TXT*sizeof(char));
+    int i, j;
+    char temp1, temp2;
+    a = (char*)calloc(WORD,sizeof(char));
+    b = (char*)calloc(TXT,sizeof(char));
     printf("enter your string: \n");
-    scanf("%[^\n]%*c", a);
-    scanf("%[^\n]%*c", b);
 
-    
+    for(i = 0; i < WORD;i++)
+    {
+        temp1 =getchar();
+        a[i] = temp1;
+        if(a[i]==' ' || a[i]== '\n' || a[i] =='\t')break;
+    }
+    a[i] ='\0';
+    for(j =0; j<TXT ;j++)
+    {
+        temp2 =getchar();
+        b[j] =  temp2;
+        if(b[j] == '~')break;
+    }
+    b[j] = '\0';
     printf("Gematria Sequences: ");
     GematriaSequences(a,b);
     printf("\n");
